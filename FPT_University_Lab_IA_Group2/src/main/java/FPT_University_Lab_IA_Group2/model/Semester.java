0d4519b71a428b0cab0e8bfb0f7e4206a5f5ff7f
chu0jz013 <williamkieu2003@gmail.com>
2023-03-31 04:44:33 +0000

    define model

--- FPT_University_Lab_IA_Group2/src/main/java/FPT_University_Lab_IA_Group2/model/Semester.java
+++ FPT_University_Lab_IA_Group2/src/main/java/FPT_University_Lab_IA_Group2/model/Semester.java
@@ -1,39 +1,38 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package FPT_University_Lab_IA_Group2.model;
-//
-//import java.sql.Date;
-//import java.util.List;
-//import javax.persistence.*;
-//
-///**
-// *
-// * @author chuoiz
-// */
-//@Entity
-//@Table(name = "Semester")
-//public class Semester {
-//
-//    @Id
-//    @Column(name = "semesterId")
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int semesterId;
-//
-//    @Column(name = "semesterName")
-//    private String semesterName;
-//
-//    @Column(name = "year")
-//    private int year;
-//    
-//    @Column(name = "startDate")
-//    private Date startDate;
-//    
-//    @Column(name = "endDate")
-//    private Date endDate;
-//
-//        
-//    @OneToMany
-//    private List<Grade> grades;
-//}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FPT_University_Lab_IA_Group2.model;
+
+import java.sql.Date;
+import java.util.List;
+import javax.persistence.*;
+
+/**
+ *
+ * @author chuoiz
+ */
+@Entity
+@Table(name = "Semester")
+public class Semester {
+
+    @Id
+    @Column(name = "semesterId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int semesterId;
+
+    @Column(name = "semesterName")
+    private String semesterName;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "startDate")
+    private Date startDate;
+
+    @Column(name = "endDate")
+    private Date endDate;
+
+    @OneToMany
+    private List<Grade> grades;
+}
